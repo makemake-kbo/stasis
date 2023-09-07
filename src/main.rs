683fe221b78a25b01d4053e63b1c2cd43a8143dd
Vukasin Gostovic <vukasin@gostovic.me>
2023-09-07 16:02:05 +0200

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,4 @@
-//! Implements a hello-world example for Arbitrum Stylus, providing a Solidity ABI-equivalent
-//! Rust implementation of the Counter contract example provided by Foundry.
-//! Warning: this code is a template only and has not been audited.
-//! ```
-//! contract Counter {
-//!     uint256 public number;
-//!     function setNumber(uint256 newNumber) public {
-//!         number = newNumber;
-//!     }
-//!     function increment() public {
-//!         number++;
-//!     }
-//! }
-//! ```
+// Implements a basic state channel layer2
 
 // Only run this as a WASM if the export-abi feature is not set.
 #![cfg_attr(not(feature = "export-abi"), no_main)]
@@ -24,36 +11,41 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 /// Import the Stylus SDK along with alloy primitive types for use in our program.
 use stylus_sdk::{alloy_primitives::U256, prelude::*};
 
-// Define the entrypoint as a Solidity storage object, in this case a struct
-// called `Counter` with a single uint256 value called `number`. The sol_storage! macro
-// will generate Rust-equivalent structs with all fields mapped to Solidity-equivalent
-// storage slots and types.
+// Define storage structs we'll need.
 sol_storage! {
+    // does stylus even pack structs?
+    pub struct Channel {
+        address counterparty0;
+        address counterparty1;
+        uint64 timestamp;
+        uint32 asset_id;
+        uint256 balance0;
+        uint256 balance1;
+    }
+
     #[entrypoint]
-    pub struct Counter {
-        uint256 number;
+    pub struct Stasis {
+        mapping(address => Channel) channels;
+        mapping(uint32 => address) asset_ids;
     }
 }
 
 /// Define an implementation of the generated Counter struct, defining a set_number
 /// and increment method using the features of the Stylus SDK.
 #[external]
-impl Counter {
-    /// Gets the number from storage.
-    pub fn number(&self) -> Result<U256, Vec<u8>> {
-        Ok(self.number.get())
+impl Stasis {
+    pub fn register_asset(
+        &mut self, 
+        new_asset: Address,
+        index: u32
+    ) -> Result<(), Vec<u8>> {
+        if self.asset_ids.contains_key(index) {
+            return Err(b"Asset already registered");
+        }
+
+        self.asset_ids.insert(index, new_asset);
     }
 
-    /// Sets a number in storage to a user-specified value.
-    pub fn set_number(&mut self, new_number: U256) -> Result<(), Vec<u8>> {
-        self.number.set(new_number);
-        Ok(())
-    }
-
-    /// Increments number and updates it values in storage.
-    pub fn increment(&mut self) -> Result<(), Vec<u8>> {
-        let number = self.number.get();
-        self.number.set(number + U256::from(1));
-        Ok(())
-    }
+    
+    
 }
